5caa765d543230bb22472a128d8ba0c52925e189
XiwayBanh <xiway.banh@gmail.com>
2021-04-03 11:08:59 +0800

    added routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+stories = [
+  { title:'Mo', description: 'Hello World' },
+  { title:'Fabien', description: 'Hello Python' },
+  { title:'Brian', description: 'Hello JS' },
+]
+Story.create(stories)
